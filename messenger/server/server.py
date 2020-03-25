b5ffe78936e9cfb43df7c6c478bc6d8c91d43ae1
Artem Dronkin <adronkin@gmail.com>
2020-03-25 17:54:42 +0300

    - server.py отформатировал согласно PEP-8

--- messenger/server/server.py
+++ messenger/server/server.py
@@ -9,7 +9,7 @@ from json import JSONDecodeError
 from socket import socket, AF_INET, SOCK_STREAM
 from select import select
 from logging import getLogger
-from threading import Thread, Lock
+from threading import Thread
 from PyQt5.QtWidgets import QApplication, QMessageBox
 sys.path.append('../')
 from gui.main_window import MainWindow
@@ -20,9 +20,10 @@ from database.srv_databese import ServerDataBase
 from srv_descriptors.address import Address
 from srv_descriptors.port import Port
 from custom.srv_function import get_message, send_message
-from custom.srv_variables import MAX_QUEUE, ACTION, PRESENCE, TIME, ERROR, USER, MESSAGE, \
-    MESSAGE_TEXT, SENDER, RESPONSE_200, RESPONSE_400, EXIT, RECIPIENT, DEL_CONTACT, ACCOUNT_NAME, \
-    ADD_CONTACT, GET_CONTACTS, RESPONSE_202, DATA, GET_REGISTERED, RESPONSE_205, RESPONSE, \
+from custom.srv_variables import MAX_QUEUE, ACTION, PRESENCE, TIME, ERROR, \
+    USER, MESSAGE, MESSAGE_TEXT, SENDER, RESPONSE_200, RESPONSE_400, EXIT, \
+    RECIPIENT, DEL_CONTACT, ACCOUNT_NAME, ADD_CONTACT, GET_CONTACTS, \
+    RESPONSE_202, DATA, GET_REGISTERED, RESPONSE_205, RESPONSE, \
     PUBLIC_KEY_REQUEST, RESPONSE_511, PUBLIC_KEY
 import log.log_config
 
@@ -31,6 +32,10 @@ LOGGER = getLogger('server_logger')
 
 
 class Server(Thread, metaclass=ServerVerified):
+    """
+    The main class of the server. Accepts dictionaries, dictionaries - packages
+    from clients, processes incoming messages. Works as a separate thread.
+    """
     # Descriptors.
     address = Address()
     port = Port()
@@ -61,8 +66,9 @@ class Server(Thread, metaclass=ServerVerified):
         The method preparing a server socket.
         :return:
         """
-        LOGGER.info(f'Сервер запущен с параметрами - {self.address}:{self.port}')
-        # Create a TCP socket (AF_INET - network socket, SOCK_STREAM - work with TCP packets).
+        LOGGER.info(f'Сервер запущен - {self.address}:{self.port}.')
+        # Create a TCP socket
+        # AF_INET - network socket, SOCK_STREAM - work with TCP packets.
         server_sock = socket(AF_INET, SOCK_STREAM)
         server_sock.bind((self.address, self.port))
         server_sock.settimeout(0.5)
@@ -85,7 +91,8 @@ class Server(Thread, metaclass=ServerVerified):
             except OSError:
                 pass
             else:
-                LOGGER.info(f'Установлено соединение с клиентом {client_address}')
+                LOGGER.info(f'Установлено соединение '
+                            f'с клиентом {client_address}.')
                 client_sock.settimeout(5)
                 self.clients.append(client_sock)
 
@@ -95,8 +102,8 @@ class Server(Thread, metaclass=ServerVerified):
             try:
                 if self.clients:
                     # Request information on readiness for input and output.
-                    clients_senders, self.listen_sockets, self.error_sockets = select(
-                        self.clients, self.clients, [], 0)
+                    clients_senders, self.listen_sockets, self.error_sockets = \
+                        select(self.clients, self.clients, [], 0)
             except OSError as err:
                 LOGGER.info(f'Ошибка работы с сокетами: {err.error}.')
 
@@ -106,7 +113,9 @@ class Server(Thread, metaclass=ServerVerified):
                 for client_with_message in clients_senders:
                     try:
                         self.processing_message(
-                            get_message(client_with_message), client_with_message)
+                            get_message(client_with_message),
+                            client_with_message
+                        )
                     except (OSError, JSONDecodeError, TypeError):
                         self.remove_client(client_with_message)
 
@@ -128,12 +137,14 @@ class Server(Thread, metaclass=ServerVerified):
 
         elif message[RECIPIENT] in self.names \
                 and self.names[message[RECIPIENT]] not in self.listen_sockets:
-            LOGGER.error(f'Доставка невозможна. Связь с клиентом {message[RECIPIENT]} потеряна.')
+            LOGGER.error(f'Доставка невозможна. '
+                         f'Связь с клиентом {message[RECIPIENT]} потеряна.')
             self.remove_client(self.names[message[RECIPIENT]])
 
         else:
-            LOGGER.error(f'Отправка сообщения невозможна. Пользователь с именем'
-                         f' {message[RECIPIENT]} не зарегистрирован на сервере.')
+            LOGGER.error(f'Отправка сообщения невозможна. '
+                         f'Пользователь {message[RECIPIENT]} '
+                         f'не зарегистрирован на сервере.')
 
     def processing_message(self, message, client):
         """
@@ -150,11 +161,12 @@ class Server(Thread, metaclass=ServerVerified):
             self.user_authorization(message, client)
 
         # If message.
-        elif ACTION in message and message[ACTION] == MESSAGE and TIME in message \
-                and SENDER in message and RECIPIENT in message and MESSAGE_TEXT in message \
+        elif ACTION in message and message[ACTION] == MESSAGE \
+                and SENDER in message and RECIPIENT in message \
+                and MESSAGE_TEXT in message \
                 and client == self.names[message[SENDER]]:
             if message[RECIPIENT] in self.names:
-                # Сохраняем сообщение в БД
+                # Save the message to the database.
                 self.database.save_message(message[SENDER],
                                            message[RECIPIENT],
                                            message[MESSAGE_TEXT])
@@ -165,7 +177,8 @@ class Server(Thread, metaclass=ServerVerified):
                     self.remove_client(client)
             else:
                 server_answer = RESPONSE_400
-                server_answer[ERROR] = f'Пользователь {message[RECIPIENT]} не зарегистрирован.'
+                server_answer[ERROR] = f'Пользователь {message[RECIPIENT]}' \
+                                       f' не зарегистрирован.'
                 try:
                     send_message(client, server_answer)
                 except OSError:
@@ -173,23 +186,25 @@ class Server(Thread, metaclass=ServerVerified):
             return
 
         # If the user wants to shut down.
-        elif ACTION in message and message[ACTION] == EXIT and TIME in message and USER in message \
+        elif ACTION in message and message[ACTION] == EXIT and USER in message \
                 and client == self.names[message[USER]]:
             self.remove_client(client)
 
         # If the user requests a contact list.
-        elif ACTION in message and message[ACTION] == GET_CONTACTS and USER in message \
+        elif ACTION in message and message[ACTION] == GET_CONTACTS \
+                and USER in message \
                 and self.names[message[USER]] == client:
             server_answer = RESPONSE_202
             server_answer[DATA] = self.database.get_contact_list(message[USER])
-            LOGGER.debug(f'Пользователь {message[USER]} запросил список контактов.')
+            LOGGER.debug(f'Пользователь {message[USER]} '
+                         f'запросил список контактов.')
             try:
                 send_message(client, server_answer)
             except OSError:
                 self.remove_client(client)
 
         # If the user adds to the contact list.
-        elif ACTION in message and message[ACTION] == ADD_CONTACT and TIME in message \
+        elif ACTION in message and message[ACTION] == ADD_CONTACT \
                 and USER in message and ACCOUNT_NAME in message \
                 and client == self.names[message[USER]]:
             self.database.add_contact(message[USER], message[ACCOUNT_NAME])
@@ -201,7 +216,7 @@ class Server(Thread, metaclass=ServerVerified):
                 self.remove_client(client)
 
         # If the user removes from the contact list.
-        elif ACTION in message and message[ACTION] == DEL_CONTACT and TIME in message \
+        elif ACTION in message and message[ACTION] == DEL_CONTACT \
                 and USER in message and ACCOUNT_NAME in message \
                 and client == self.names[message[USER]]:
             self.database.del_contact(message[USER], message[ACCOUNT_NAME])
@@ -213,11 +228,10 @@ class Server(Thread, metaclass=ServerVerified):
                 self.remove_client(client)
 
         # If the user requests a list of known users.
-        elif ACTION in message and message[ACTION] == GET_REGISTERED and USER in message \
-                and self.names[message[USER]] == client:
+        elif ACTION in message and message[ACTION] == GET_REGISTERED \
+                and USER in message and self.names[message[USER]] == client:
             server_answer = RESPONSE_202
             server_answer[DATA] = [user[0] for user in self.database.get_all_users()]
-            # TODO оптимизировать запрос.
             LOGGER.debug(f'Пользователь {message[USER]} запросил список'
                          f' зарегистрированных пользователей.')
             try:
@@ -226,7 +240,8 @@ class Server(Thread, metaclass=ServerVerified):
                 self.remove_client(client)
 
         # If the user requests a public key.
-        elif ACTION in message and message[ACTION] == PUBLIC_KEY_REQUEST and USER in message:
+        elif ACTION in message and message[ACTION] == PUBLIC_KEY_REQUEST \
+                and USER in message:
             server_answer = RESPONSE_511
             server_answer[DATA] = self.database.get_key(message[USER])
             # If there is no key yet, send 400 (the user has never logged in).
@@ -246,8 +261,9 @@ class Server(Thread, metaclass=ServerVerified):
         else:
             server_answer = RESPONSE_400
             server_answer[ERROR] = 'Bad request.'
-            LOGGER.debug(f'Получен некорректный запрос от клиента {client.getpeername()},'
-                         f' отправлен ответ {server_answer}.')
+            LOGGER.debug(f'Получен некорректный запрос от клиента'
+                         f' {client.getpeername()}, отправлен ответ'
+                         f' {server_answer}.')
             try:
                 send_message(client, server_answer)
             except OSError:
@@ -264,14 +280,16 @@ class Server(Thread, metaclass=ServerVerified):
         if message[USER] in self.names.keys():
             server_answer = RESPONSE_400
             server_answer[ERROR] = 'Имя пользователя занято.'
-            LOGGER.error(f'Попытка подключения клиента {sock.getpeername()} используя'
-                         f' занятое имя пользователя. Отправлен ответ {RESPONSE_400}.')
+            LOGGER.error(f'Попытка подключения клиента {sock.getpeername()} '
+                         f'используя занятое имя пользователя. '
+                         f'Отправлен ответ {RESPONSE_400}.')
             try:
                 send_message(sock, server_answer)
             except OSError:
                 pass
             self.clients.remove(sock)
             sock.close()
+
         # Check that the user is registered on the server.
         if not self.database.check_user(message[USER]):
             server_answer = RESPONSE_400
@@ -282,6 +300,7 @@ class Server(Thread, metaclass=ServerVerified):
                 pass
             self.clients.remove(sock)
             sock.close()
+
         # Otherwise, answer 511 and start the authorization procedure.
         else:
             auth_answer = RESPONSE_511
@@ -291,7 +310,10 @@ class Server(Thread, metaclass=ServerVerified):
             auth_answer[DATA] = random_str.decode('ascii')
             # Create a password hash and a string with a random string,
             # save the server version of the key.
-            password_hash = hmac.new(self.database.get_pass_hash(message[USER]), random_str)
+            password_hash = hmac.new(
+                self.database.get_pass_hash(message[USER]),
+                random_str
+            )
             digest = password_hash.digest()
 
             try:
@@ -301,7 +323,8 @@ class Server(Thread, metaclass=ServerVerified):
                 sock.close()
                 return
             client_digest = a2b_base64(answer[DATA])
-            # If the client’s answer is correct, then save it to the list of users.
+
+            # If the client’s answer is correct, save it in the user list.
             if RESPONSE in answer and answer[RESPONSE] == 511 \
                     and hmac.compare_digest(digest, client_digest):
                 self.names[message[USER]] = sock
@@ -310,8 +333,14 @@ class Server(Thread, metaclass=ServerVerified):
                     send_message(sock, RESPONSE_200)
                 except OSError:
                     self.remove_client(message[USER])
-                # Add user to active list. Save the public key if it has changed.
-                self.database.user_login(message[USER], client_ip, client_port, message[PUBLIC_KEY])
+                # Add user to active list.
+                # Save the public key if it has changed.
+                self.database.user_login(
+                    message[USER],
+                    client_ip,
+                    client_port,
+                    message[PUBLIC_KEY]
+                )
             else:
                 server_answer = RESPONSE_400
                 server_answer[ERROR] = 'Неверный пароль.'
@@ -330,7 +359,7 @@ class Server(Thread, metaclass=ServerVerified):
         :return:
         """
         LOGGER.info(f'Клиент {client.getpeername()} отключился от сервера.')
-        # Ищем и удаляем клиента из словаря клиентов.
+        # Find and remove a customer from the self.clients dict.
         for name in self.names:
             if self.names[name] == client:
                 self.database.user_logout(name)
@@ -354,29 +383,31 @@ class Server(Thread, metaclass=ServerVerified):
 
 def main():
     """
-    Запуск сервера.
+    Starting the server.
     :return:
     """
-    # Загружаем файл конфигурации сервера.
+    # Download the server configuration file.
     config = ConfigParser()
 
     dir_path = os.path.dirname(os.path.realpath(__file__))
     config.read(f'{dir_path}/server.ini')
 
-    # Парсим параметры из командной строки или установка значений по умолчанию.
-    listen_address, listen_port = args_parser(config['SETTINGS']['default_ip'],
-                                              config['SETTINGS']['default_port'])
+    # Parsing parameters from the command line or setting default values.
+    listen_address, listen_port = args_parser(
+        config['SETTINGS']['default_ip'],
+        config['SETTINGS']['default_port']
+    )
 
-    # Инициализируем БД.
+    # Database initialization.
     database = ServerDataBase(os.path.join(config['SETTINGS']['db_path'],
                                            config['SETTINGS']['db_file']))
 
-    # Создаем экземпляр сервера.
+    # Create a server instance.
     server = Server(listen_address, listen_port, database)
     server.daemon = True
     server.start()
 
-    # Создаем графическое окружение для сервера.
+    # Create a graphical environment for the server.
     server_app = QApplication(sys.argv)
     main_window = MainWindow(server, database)
 
@@ -385,43 +416,50 @@ def main():
         Функция создает окно настроек сервера.
         :return:
         """
-        global config_window
-        config_window = ConfigWindow()
-        config_window.db_path.insert(config['SETTINGS']['db_path'])
-        config_window.db_file_name.insert(config['SETTINGS']['db_file'])
-        config_window.ip_address_field.insert(config['SETTINGS']['default_ip'])
-        config_window.port_field.insert(str(config['SETTINGS']['default_port']))
-        config_window.save_button.clicked.connect(save_server_config)
+        global CONFIG_WINDOW
+        CONFIG_WINDOW = ConfigWindow()
+        CONFIG_WINDOW.db_path.insert(config['SETTINGS']['db_path'])
+        CONFIG_WINDOW.db_file_name.insert(config['SETTINGS']['db_file'])
+        CONFIG_WINDOW.ip_address_field.insert(config['SETTINGS']['default_ip'])
+        CONFIG_WINDOW.port_field.insert(str(config['SETTINGS']['default_port']))
+        CONFIG_WINDOW.save_button.clicked.connect(save_server_config)
 
     def save_server_config():
         """
         Функция сохранения настроек.
         :return:
         """
-        global config_window
+        global CONFIG_WINDOW
         message = QMessageBox()
-        config['SETTINGS']['db_path'] = config_window.db_path.text()
-        config['SETTINGS']['db_file'] = config_window.db_file_name.text()
+        config['SETTINGS']['db_path'] = CONFIG_WINDOW.db_path.text()
+        config['SETTINGS']['db_file'] = CONFIG_WINDOW.db_file_name.text()
         try:
-            port = int(config_window.port_field.text())
+            port = int(CONFIG_WINDOW.port_field.text())
         except ValueError:
-            message.warning(config_window, 'Ошибка', 'Порт должен быть числом.')
+            message.warning(CONFIG_WINDOW, 'Ошибка', 'Порт должен быть числом.')
         else:
             # TODO добавить проверку корректности IP-адреса.
-            config['SETTINGS']['default_ip'] = config_window.ip_address_field.text()
+            config['SETTINGS']['default_ip'] = CONFIG_WINDOW.ip_address_field.text()
             if 1023 < port < 65536:
-                config['SETTINGS']['default_port'] = config_window.port_field.text()
-                # Записываем новы порт в файл.
+                config['SETTINGS']['default_port'] = CONFIG_WINDOW.port_field.text()
+                # Write a new port to a file.
                 with open(os.path.join(dir_path, 'server.ini'), 'w') as conf:
                     config.write(conf)
-                    message.information(config_window, 'Ok', 'Настройки сохранены.')
+                    message.information(
+                        CONFIG_WINDOW,
+                        'Ok',
+                        'Настройки сохранены.'
+                    )
             else:
-                message.warning(config_window, 'Ошибка', 'Порт должен быть от 1024 до 65536.')
+                message.warning(CONFIG_WINDOW,
+                                'Ошибка',
+                                'Порт должен быть от 1024 до 65536.'
+                                )
 
-    # Связываем кнопки с процедурами.
+    # Associate buttons with procedures.
     main_window.setting_button.triggered.connect(server_config)
 
-    # Запускаем GUI.
+    # Run GUI.
     server_app.exec_()
 
     # By closing the windows, we stop the message handler.
@@ -432,4 +470,4 @@ if __name__ == '__main__':
     main()
 
 # TODO сервер падает если завершить работу клиента ctrl+D.
-# TODO удается зарегать пользователя без пароля.
+# TODO удается зарегистрировать пользователя без пароля.
